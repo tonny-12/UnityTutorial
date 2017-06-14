6568530c3e2d7d4d4a1eb4d1384a6a8dd6ec5174
Tonny <tonnyx1294@hotmail.com>
2017-06-14 08:49:01 -0400

    added files

---
+++ Completed/Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Snake : MonoBehaviour {
+
+	// direction of Snake's movement
+    private Vector2 direction;
+
+	// Snake's Tail Prefab for eating Food
+	public GameObject tailPrefab;
+
+	// keeping track of the tail
+	private List<Transform> tailList;
+
+	// flag for checking if the Snake ate Food
+	private bool ate;
+
+	public string upButton;
+	public string downButton;
+	public string rightButton;
+	public string leftButton;
+
+	public Vector2 initialDirection;
+
+	public Text gameOverText;
+	public Text restartText;
+
+	private bool gameOver;
+	private bool restart;
+
+	// Use this for initialization 
+	void Start () {
+		gameOver = false;
+		restart = false;
+		gameOverText.text = "";
+		restartText.text = "";
+
+		// Snake starts with being hungry
+		ate = false;
+
+		// initastiating the List
+		tailList = new List<Transform>();
+
+        // Snake starts moving to the right
+		direction = initialDirection;
+
+		// Snake moves every 100ms by calling the Move() function
+        InvokeRepeating("Move", 0.1f, 0.1f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // Snake direction changes based on pressed key and NO TURNING BACK
+		//if (Input.GetKey(KeyCode.RightArrow) && (direction != Vector2.left))
+		if (Input.GetButton(rightButton) && (direction != Vector2.left))
+        {
+            direction = Vector2.right;
+        }
+		else if (Input.GetButton(leftButton) && (direction != Vector2.right))
+       	{
+            direction = Vector2.left;
+        }
+		else if (Input.GetButton(upButton) && (direction != Vector2.down))
+        {
+            direction = Vector2.up;
+        }
+		else if (Input.GetButton(downButton) && (direction != Vector2.up))
+        {
+            direction = Vector2.down;
+        }
+
+		if (restart) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex);
+			}
+		}
+		if (gameOver) {
+			restartText.text = "Press Space for Restart";
+			restart = true;
+		}
+    }
+
+	// Move used to move the Snake
+    void Move()
+    {
+		// save current position of the Snake's Head
+		Vector2 v = transform.position;
+
+		// move Snake's Head one space based on the new direction (gap)
+        transform.Translate(direction);
+
+		// check if the ate flag is true
+		if (ate) {
+			// loads the Tail Prefab to be placed where the Snake's Head was (fills gap)
+			GameObject gameObject = (GameObject)Instantiate (tailPrefab, v, Quaternion.identity);
+
+			// adds the loaded Tail Prefab to the Tail list
+			tailList.Insert (0, gameObject.transform);
+
+			// Snake finished eating setting ate flag back to false
+			ate = false;
+		}
+		// check if the Snake have a Tail
+		else if (tailList.Count > 0) 
+		{
+			// move last Tail element to where the Head was
+			tailList.Last ().position = v;
+
+			// add Tail to the front of the list
+			tailList.Insert (0, tailList.Last ());
+
+			// remove Tail from the back of the list
+			tailList.RemoveAt (tailList.Count - 1);
+		}
+
+    }
+
+	// Trigger event when another object collides with this object
+	void OnTriggerEnter2D(Collider2D other) 
+	{	
+		// checks to see if collided object has the Food tag
+		if (other.CompareTag("Food"))
+		{	
+			// set ate flag to true
+			ate = true;
+			// destroy the Food
+			Destroy (other.gameObject);
+		}
+		// Snake is detroyed if collided with any other object
+		else 
+		{
+			Destroy (gameObject);
+		}
+	}
+}
